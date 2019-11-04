fa06ac22a70badc88d448c5a3ca095b29ddea98d
purpleBrain <timujin08@yandex.ru>
2019-11-04 14:56:45 +0300

    webpack and co setup

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключили плагин
+
+module.exports = {
+    entry: { main: './src/index.js' },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[chunkhash].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+    {
+                test: /\.css$/,
+                use:  [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+             }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({filename: 'style.[contenthash].css'}),
+        new HtmlWebpackPlugin({ // настроили плагин
+            inject: false,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ]
+};
