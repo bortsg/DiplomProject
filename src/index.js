da8ce9b4db64a7abf4c346c7e8257940308c40d2
purpleBrain <timujin08@yandex.ru>
2020-01-07 08:59:07 +0300

    js block results added

--- src/index.js
+++ src/index.js
@@ -21,16 +21,24 @@ function showResultsBlock() {
   // До получения данных блок содержит:
   // заголовок «Результаты»,
   // прелоудер.
+
+  document.querySelector('.results__content').setAttribute('style', 'display:none');
+  document.querySelector('.results__analytics').setAttribute('style', 'display:none');
+  document.querySelector('.results__show-more').setAttribute('style', 'display:none');
+  document.querySelector('.not-found').setAttribute('style', 'display:none');
 }
 
 function showNotFoundResults() {
   // если ничего не найдено (data.results.length == 0) - на месте прелоудера надпись «Ничего не найдено»
+  document.querySelector('.preloader').setAttribute('style', 'display:none');
+  document.querySelector('.not-found').setAttribute('style', 'display:flex');
 }
 
 function showCards() {
   // Когда данные получены, в блоке результатов исчезает прелоудер, и появляются карточки.
   // Их следует расположить в линию по 3 на каждой строке на разрешении 1280 пикселей.
   // Если сжимать окно браузера, карточки переносятся на следующую строку.
+  document.querySelector('.preloader').setAttribute('style', 'display:none');
 
 
   // Если карточек больше трёх, отрисовываются только 3, а под ними появляется кнопка «Показать ещё».
@@ -41,6 +49,7 @@ function showCards() {
 function showErrorResults() {
   // в окне результатов выводится надпись «Во время запроса произошла ошибка. Возможно, проблема
   // с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз».
+
 }
 
 document.querySelector('.search__field').addEventListener('submit', () => {
@@ -54,7 +63,7 @@ document.querySelector('.search__field').addEventListener('submit', () => {
     .then(function (res) {
       if (res.ok) {
 
-        //showResultsBlock();
+        showResultsBlock();
 
         console.log('ok');
         return res.json();
@@ -63,9 +72,14 @@ document.querySelector('.search__field').addEventListener('submit', () => {
     })
     .then((data) => {
       console.log(data);
+      console.log(data.totalResults);
       // настроить хранение localStorage
 
-      // showNotFoundResults();
+      if (data.totalResults===0) {
+      // если ничего не найдено
+        showNotFoundResults();
+      }
+
 
       // showCards();
     })
