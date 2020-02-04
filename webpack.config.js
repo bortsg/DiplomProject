0f8bf76252349b38efd67f759fc09476dae3881c
bortsg <timujin08@yandex.ru>
2020-02-04 12:37:35 +0400

    css and images paths updated with subfolders

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
       {
         test: /\.(png|jpe?g|gif|ico|svg)$/i,
         use: [
-          'file-loader?name=./images/[name].[ext]',
+          'file-loader?name=../dist/images/[name].[ext]',
           {
             loader: 'image-webpack-loader',
             options: {
@@ -35,7 +35,7 @@ module.exports = {
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./fonts/[name].[ext]'
+        loader: 'file-loader?name=../dist/fonts/[name].[ext]'
       },
       {
         test: /\.js$/,
