c4857b03b0cf60473cb1afce571f1883b8ba706b
purpleBrain <timujin08@yandex.ru>
2020-02-18 23:20:58 +0300

    results grid modified to fractions

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -8,38 +8,29 @@ export default class NewsCard {
     const templ = document.getElementById('templ');
     const elem = templ.content.cloneNode(true);
 
-    const card = document.createElement('article');
-    card.classList.add('news-item');
-    elem.appendChild(card);
+    // const card = document.createElement('article');
+    // card.classList.add('news-item');
+    // elem.appendChild(card);
 
-    const newsImage = document.createElement('img');
-    newsImage.classList.add('news-item__image');
+    // const newsImage = document.createElement('img');
+    const newsImage = elem.querySelector('.news-item__image');
     newsImage.setAttribute('src', JSON.parse(localStorage.getItem(0)).urlToImage);
-    newsImage.setAttribute('alt', 'Новостная иллюстрация');
-    card.appendChild(newsImage);
 
-    const newsDate = document.createElement('div');
-    newsDate.classList.add('news-item__date');
+    const newsDate = elem.querySelector('.news-item__date');
     newsDate.textContent = JSON.parse(localStorage.getItem(0)).publishedAt;
-    card.appendChild(newsDate);
 
-    const newsTitle = document.createElement('h3');
-    newsTitle.classList.add('news-item__title');
+    const newsTitle = elem.querySelector('.news-item__title');
     newsTitle.textContent = JSON.parse(localStorage.getItem(1)).title;
-    card.appendChild(newsTitle);
 
-    const newsDescription = document.createElement('p');
-    newsDescription.classList.add('news-item__description');
+    const newsDescription = elem.querySelector('.news-item__description');
     newsDescription.textContent = JSON.parse(localStorage.getItem(0)).description;
-    card.appendChild(newsDescription);
 
-    const newsUrl = document.createElement('a');
-    newsUrl.classList.add('news-item__source');
-    newsUrl.textContent = JSON.parse(localStorage.getItem(0)).author;
-    newsUrl.setAttribute('href', JSON.parse(localStorage.getItem(0)).source.name);
-    card.appendChild(newsUrl);
-    
-    console.log(elem);
+    const newsUrl = elem.querySelector('.news-item__source')
+    newsUrl.textContent = JSON.parse(localStorage.getItem(0)).source.name;
+    newsUrl.setAttribute('target', 'blank');
+    newsUrl.setAttribute('href', JSON.parse(localStorage.getItem(0)).url);
+
+    // console.log(elem);
 
     document.querySelector('.results__content').appendChild(elem);
 
@@ -47,11 +38,11 @@ export default class NewsCard {
     document.querySelector('.results__content').setAttribute('style', 'display:grid');
     document.querySelector('.results__analytics').setAttribute('style', 'display:flex');
     document.querySelector('.results__show-more').setAttribute('style', 'display:inline');
-    
-    
-    
-    
-    
+
+
+
+
+
 
   }
 }
