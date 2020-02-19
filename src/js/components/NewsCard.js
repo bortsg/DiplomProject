ad297d27e75cc868c1fabdd6fd995a394316a519
purpleBrain <timujin08@yandex.ru>
2020-02-19 17:05:52 +0300

    add button is wrong

--- src/js/components/NewsCard.js
+++ src/js/components/NewsCard.js
@@ -1,48 +1,66 @@
 export default class NewsCard {
   constructor() {
-    // this.img = newsItem.urlToImage;
-    // console.log(img);
+  }
+
+  draw(k) {
+    for (let i=0; i<3; i++) {
+      // добавить функцию проверки наличия, и корректности картинки изображения новости
+      // console.log(fetch(JSON.parse(localStorage.getItem(i+3*k)).urlToImage, {method: 'GET'}) );
+
+      if (localStorage.getItem(i+3*k)) {
+        const templ = document.getElementById('templ');
+        const elem = templ.content.cloneNode(true);
+
+        const data = JSON.parse(localStorage.getItem(i+3*k)).publishedAt;
+        const parsedDate = Date.parse(data);
+        const formatter = new Intl.DateTimeFormat("ru", {year: "numeric", month: "long", day: "numeric"});
+        const formattedDate = formatter.format(new Date(parsedDate));
+        const publishedDate = formattedDate.slice(0, formattedDate.length-8) + ", " + formattedDate.slice(-7,-3);
+
+        const newsImage = elem.querySelector('.news-item__image');
+        if (JSON.parse(localStorage.getItem(i+3*k)).urlToImage) {
+          newsImage.setAttribute('src', JSON.parse(localStorage.getItem(i+3*k)).urlToImage);
+        } else newsImage.setAttribute('src', 'https://www.funnycatpix.com/_pics/Sitting_With_My_Kids.jpg');
+
+        const newsDate = elem.querySelector('.news-item__date');
+        newsDate.textContent = publishedDate;
+
+        const newsTitle = elem.querySelector('.news-item__title');
+        newsTitle.textContent = JSON.parse(localStorage.getItem(i+3*k)).title;
+
+        const newsDescription = elem.querySelector('.news-item__description');
+        newsDescription.textContent = JSON.parse(localStorage.getItem(i+3*k)).description;
+
+        const newsUrl = elem.querySelector('.news-item__source')
+        newsUrl.textContent = JSON.parse(localStorage.getItem(i+3*k)).source.name;
+        newsUrl.setAttribute('target', 'blank');
+        newsUrl.setAttribute('href', JSON.parse(localStorage.getItem(i+3*k)).url);
+
+        document.querySelector('.results__content').appendChild(elem);
+      }
+    }
   }
 
   create() {
-    const templ = document.getElementById('templ');
-    const elem = templ.content.cloneNode(true);
+    let k=0;
+    this.draw(k);
 
-    // const card = document.createElement('article');
-    // card.classList.add('news-item');
-    // elem.appendChild(card);
-
-    // const newsImage = document.createElement('img');
-    const newsImage = elem.querySelector('.news-item__image');
-    newsImage.setAttribute('src', JSON.parse(localStorage.getItem(0)).urlToImage);
-
-    const newsDate = elem.querySelector('.news-item__date');
-    newsDate.textContent = JSON.parse(localStorage.getItem(0)).publishedAt;
-
-    const newsTitle = elem.querySelector('.news-item__title');
-    newsTitle.textContent = JSON.parse(localStorage.getItem(1)).title;
-
-    const newsDescription = elem.querySelector('.news-item__description');
-    newsDescription.textContent = JSON.parse(localStorage.getItem(0)).description;
-
-    const newsUrl = elem.querySelector('.news-item__source')
-    newsUrl.textContent = JSON.parse(localStorage.getItem(0)).source.name;
-    newsUrl.setAttribute('target', 'blank');
-    newsUrl.setAttribute('href', JSON.parse(localStorage.getItem(0)).url);
-
-    // console.log(elem);
-
-    document.querySelector('.results__content').appendChild(elem);
+    if ( k < 2) {
+      document.querySelector('.results__show-more').setAttribute('style', 'display:inline');
+      document.querySelector('.results__show-more').addEventListener('click', () => {
+        console.log(k);
+        k = k+1;
+        this.draw(k);
+      })
+    } else {
+      console.log('hopa');
+      // document.querySelector('.results__show-more').removeEventListener();
+      document.querySelector('.results__show-more').setAttribute('style', 'display:none');
+    }
 
     document.querySelector('.results').setAttribute('style', 'display:flex');
     document.querySelector('.results__content').setAttribute('style', 'display:grid');
     document.querySelector('.results__analytics').setAttribute('style', 'display:flex');
-    document.querySelector('.results__show-more').setAttribute('style', 'display:inline');
-
-
-
-
-
-
+    // document.querySelector('.results__show-more').setAttribute('style', 'display:inline');
   }
 }
